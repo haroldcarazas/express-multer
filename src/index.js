fd2239469fca305e800e231c514afa4ba35f8c47
harold2001 <hcarazasnires@gmail.com>
2024-03-19 11:15:04 -0500

    {feature}: ruta para mostrar una imagen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { PORT } from './config.js'
 import multer from 'multer'
+import path from 'node:path'
+import fs from 'node:fs'
 
 const app = express()
 
@@ -15,7 +17,7 @@ const storage = multer.diskStorage({
 })
 
 // Configurar la carga de archivos
-const upload = multer({
+const uploadImage = multer({
   storage,
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
@@ -26,18 +28,42 @@ const upload = multer({
   }
 })
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' })
-})
-
 // Función para manejar el error
 const handleError = (err, req, res, next) => {
   console.log(err)
   res.status(400).json({ error: 'Solo se admiten imágenes' })
 }
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' })
+})
+
+// Ruta para mostrar una imagen
+app.get('/image/:filename', (req, res) => {
+  try {
+    const { filename } = req.params
+    const absolutePath = path.resolve(`./uploads/${filename}`)
+
+    fs.access(absolutePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.status(404).json({ message: 'Imagen no encontrada' })
+      } else {
+        res.sendFile(absolutePath)
+      }
+    })
+  } catch (error) {
+    res.json({ message: error.message })
+  }
+})
+
 // Ruta para subir archivos y manejar el error de cuando no es una imagen
-app.post('/upload', upload.single('imagen'), handleError, async (req, res) => {
+app.post('/upload', uploadImage.single('imagen'), handleError, async (req, res) => {
   res.json({ message: 'Archivo subido correctamente' })
 })
 
